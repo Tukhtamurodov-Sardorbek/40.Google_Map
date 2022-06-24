df5fb806d49eed48fec5a0f9172874741eeb2ca3
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-06-24 21:27:45 +0500

    🛡 Code Optimized 🛡

---
+++ lib/pages/internet_connection/view.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_map_integration/pages/home/view.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+
+import 'provider.dart';
+
+class InternetConnectionPage extends StatefulWidget {
+  const InternetConnectionPage({Key? key}) : super(key: key);
+
+  @override
+  State<InternetConnectionPage> createState() => _InternetConnectionPageState();
+}
+
+class _InternetConnectionPageState extends State<InternetConnectionPage> {
+  @override
+  void initState() {
+    Provider.of<InternetConnectionProvider>(context, listen: false).listenTheStatus();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = context.watch<InternetConnectionProvider>();
+    print('In build: ${provider.hasInternet}');
+    return provider.hasInternet ? const HomePage() : const NoInternet();
+  }
+
+}
+
+class NoInternet extends StatelessWidget {
+  const NoInternet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 300,
+              child: LottieBuilder.asset(
+                  'assets/lottie/no_internet_connection.json'),
+            ),
+            // const SizedBox(height: 10),
+            // ElevatedButton(
+            //   onPressed: () async {
+            //     provider.hasInternet =
+            //     await InternetConnectionChecker().hasConnection;
+            //   },
+            //   style: ElevatedButton.styleFrom(
+            //     padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            //     shape: const StadiumBorder(),
+            //   ),
+            //   child: const Icon(
+            //     CupertinoIcons.arrow_counterclockwise,
+            //     size: 30,
+            //   ),
+            // )
+          ],
+        ),
+      ),
+    );
+  }
+}
