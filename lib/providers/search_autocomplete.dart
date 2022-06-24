e1174a3d03134340620e8d5d2692f10d996214af
Tukhtamurodov Sardorbek <96657039+Tukhtamurodov-Sardorbek@users.noreply.github.com>
2022-06-24 23:03:19 +0500

    🛡 Code Optimized 🛡

--- lib/providers/search_autocomplete.dart
+++ lib/providers/search_autocomplete.dart
@@ -2,54 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:google_map_integration/models/autocomplete_model.dart';
 
 class PlaceResults extends ChangeNotifier {
-  // List<AutoCompleteResult> _allAutocompleteResults = [];
-  //
-  // List<AutoCompleteResult> get searchResults => _allAutocompleteResults;
-  //
-  // set searchResults(allPlaces) {
-  //   _allAutocompleteResults = allPlaces;
-  //   notifyListeners();
-  // }
+  List<AutoCompleteResult> _allAutocompleteResults = [];
 
-  List<AutoCompleteResult> allReturnedResults = [];
+  List<AutoCompleteResult> get searchResults => _allAutocompleteResults;
 
-  void setResults(List<AutoCompleteResult> allPlaces) {
-    allReturnedResults = allPlaces;
+  set searchResults(List<AutoCompleteResult> allPlaces) {
+    _allAutocompleteResults = allPlaces;
     notifyListeners();
   }
 }
 
 class SearchToggle extends ChangeNotifier {
-  // bool _searchToggle = false;
-  //
-  // bool get searchToggle => _searchToggle;
-  //
-  // void toggleSearch() {
-  //   _searchToggle = !_searchToggle;
-  //   notifyListeners();
-  // }
-  bool searchToggle = false;
+  bool _searchToggle = false;
+
+  bool get searchToggle => _searchToggle;
 
   void toggleSearch() {
-    searchToggle = !searchToggle;
+    _searchToggle = !_searchToggle;
     notifyListeners();
   }
 }
 
 class FromToggle extends ChangeNotifier {
-  bool fromToggle = false;
+  bool _fromToggle = false;
+
+  bool get fromToggle => _fromToggle;
 
   void toggleFrom() {
-    fromToggle = !fromToggle;
+    _fromToggle = !_fromToggle;
     notifyListeners();
   }
 }
 
 class ToToggle extends ChangeNotifier {
-  bool toToggle = false;
+  bool _toToggle = false;
+
+  bool get toToggle => _toToggle;
 
   void toggleTo() {
-    toToggle = !toToggle;
+    _toToggle = !_toToggle;
     notifyListeners();
   }
 }
